c25c689d45d362f577464ee21ee83f85de210495
VitorPaes <vitor.mor66@live.com>
2021-12-05 21:54:49 -0300

    Continuando

--- Alura/Python/Curso 1/forca.py
+++ Alura/Python/Curso 1/forca.py
@@ -4,5 +4,22 @@ def jogar():
     print("    Bem vindo ao jogo da forc    ")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+
+    #enquanto True and True
+    while(not enforcou and not acertou ):
+        chute = input("Qual letra?  ")
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Econtrei a letra {} na posicao {}".format(letra,index))
+            index = index + 1
+        print("jogando")
+    	
+    print("Fim do Jogo")
+
 if(__name__ == "__main__"):
     jogar()

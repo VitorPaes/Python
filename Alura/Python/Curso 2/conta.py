898bb6a13d69c07acf292f843a68cf826d8602df
VitorPaes <vitor.mor66@live.com>
2021-12-11 18:51:14 -0300

    ok

--- Alura/Python/Curso 2/conta.py
+++ Alura/Python/Curso 2/conta.py
@@ -1,3 +1,21 @@
 class Conta:
 
-    pass
+    
+    def __init__(self, numero, titular, saldo, limite = 1000.0):
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        
+    def extrato(self):
+        print("Saldo {} do Titular {}".format(self.__saldo,self.__titular))        
+    
+    def deposita(self,valor):
+        self.__saldo += valor
+    
+    def saca(self,valor):
+        self.__saldo -= valor
+        
+    def transfere(self,valor,destino):
+        self.saca(valor)
+        destino.deposita(valor)

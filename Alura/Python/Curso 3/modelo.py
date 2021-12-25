965fb92a4848c1197297f4c9471a7aa84ab370d7
VitorPaes <vitor.mor66@live.com>
2021-12-24 21:45:07 -0300

    Feliz Natal

--- Alura/Python/Curso 3/modelo.py
+++ Alura/Python/Curso 3/modelo.py
@@ -40,18 +40,40 @@ class Serie(Programa):
     def __str__(self):
         return f'Nome: {self._nome} - {self.ano} - {self.temporadas} Temp - {self._likes} Likes' 
 
-vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-vingadores.dar_like()
-
-
-atlanta = Serie('atlanta', 2018, 2)
-atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - '
-      f'Temporada: {atlanta.temporadas} - Like: {atlanta.likes}' )
-
-filmes_e_series = [vingadores,atlanta]
-
-for programa in filmes_e_series:
-    print(programa)
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
     
+    @property
+    def listagem(self):
+        return self._programas
+    
+    @property
+    def tamanho(self):
+        return len(self._programas)
+        
+        
+        
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+ 
+
+vingadores.dar_like()
+vingadores.dar_like()
+demolidor.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('Fim de Semana', filmes_e_series)
+
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana.listagem:
+    print(programa)
+
+print(f' Tem demolidor? {demolidor in playlist_fim_de_semana}')

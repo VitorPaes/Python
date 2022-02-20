a80174e366d3de42fe717a49b3a0929d6b74d478
VitorPaes <vitor.mor66@live.com>
2022-02-20 15:22:52 -0300

    fim do Curso - API

--- Alura/Python/Curso 6 - Dados Brasil/main.py
+++ Alura/Python/Curso 6 - Dados Brasil/main.py
@@ -1,7 +1,19 @@
-from cpf_cnpj import Documento
 
-#cpf_um = Cpf("15316267454")
-#print(cpf_um)
-exemplo_cnpj = "35379838000112"
-documento = Documento.cria_documento(exemplo_cnpj)
-print(documento)
+
+from cpf_cnpj import Documento
+from TelefonesBr import TelefonesBr
+from datas_br import DatasBr
+from datetime import datetime, timedelta
+from acesso_cep import BuscaEnderoco
+import requests
+import re
+
+
+
+
+cep = "03726000"
+objeto_cep = BuscaEnderoco(cep)
+
+bairro, cidade, uf = objeto_cep.acessa_via_cep() 
+
+print(bairro, cidade, uf)
